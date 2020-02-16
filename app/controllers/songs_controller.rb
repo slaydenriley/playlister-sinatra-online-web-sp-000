c4f9af942da168afc8d50eaae53cd2cdcca6f35a
Riley Slayden <slaydenriley@gmail.com>
2020-02-16 20:04:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,12 +19,12 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(params[:song])     
+    @song = Song.create(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
-    @song.save 
+    @song.save
 
-    flash[:message] = "Successfully created song." 
+    flash[:message] = "Successfully created song."
     redirect("/songs/#{@song.slug}")
   end
 
